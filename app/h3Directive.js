69c3b530d9dc8b7d27b01f4376bb7a10e05ec27d
Mohammad Jakir Hossain <mjakir.ddd@gmail.com>
2016-12-09 23:49:13 +0600

    Manipulating DOM with link function

--- app/h3Directive.js
+++ app/h3Directive.js
@@ -3,9 +3,18 @@ angular.module('app').directive('h3Directive', function() {
         scope: {
             title: '@'
         },
+        restrict: 'EA',
+        replace: true,
+        template: '<h3>{{title}}</h3>',
+        link: function (scope, element, attrs) {
+            element.bind('mouseenter', function () {
+                element.css('background-color', 'red')
+            });
 
-        restrict: 'E',
-        template: '<h3>{{title}}</h3>'
+            element.bind('mouseleave', function () {
+                element.css('background-color', 'silver')
+            });
+        }
     };
    
 });
